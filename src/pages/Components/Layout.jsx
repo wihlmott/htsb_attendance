f210e204110958ab5ce7de52715def796a99b1bf
Wihlmott <wit.valentine@gmail.com>
2024-07-10 18:06:24 +0200

    layoutAdjusted

--- src/pages/Components/Layout.jsx
+++ src/pages/Components/Layout.jsx
@@ -1,39 +1,14 @@
-import { Card, Typography } from "@mui/material";
-import Layout from "../Components/Layout";
-import { Link } from "react-router-dom";
-import { allTeachers } from "../../config";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
-const FileBanner = ({ files }) => {
-  const displayClasses = (teacherCode) => files.filter((classes)=>classes.teacherCode==teacherCode);
-
-  const showFiles = displayClasses(allTeachers[0].teacherCode);
-
+const Layout = ({ children, size = 2 }) => {
   return (
-    <>
-      <Layout
-        children={showFiles.map((el) => {
-          return (
-            <Link
-              key={el.subject+el.grade}
-              style={styles.link}
-              to={`/files/${el.grade.replace(" ", "")}/${el.subject}/${
-                el.teacherCode
-              }/${el.class}`}
-            >
-              <Card sx={styles.subjectCard}>
-                <Typography variant="body1" sx={styles.text}>
-                  Grade {el.grade}{el.registerCl}
-                </Typography>
-                <Typography variant="subtitle2" sx={styles.subtitleText}>
-                  {`${el.subject} - 
-                  ${el.teacherCode}`}
-                </Typography>
-              </Card>
-            </Link>
-          );
-        })}
-      />
-    </>
+    <Grid2 container>
+      <Grid2 item="true" xs={0.4} md={size}></Grid2>
+      <Grid2 item="true" xs={11.2} md={8} sx={styles.banner}>
+        {children}
+      </Grid2>
+      <Grid2 item="true" xs={0.4} md={size}></Grid2>
+    </Grid2>
   );
 };
 
@@ -45,7 +20,6 @@ const styles = {
     borderRadius: "10px",
     overflow:'scroll'
   },
-  link: { textDecoration: "none" },
 };
 
-export default FileBanner;
+export default Layout;
